d14fc496eb489768f5e0f68f92a7e12237d64bb7
Jolin-zhang <72132138+Jolin-zhang@users.noreply.github.com>
2021-01-19 15:45:58 +0800

    Add files via upload

---
+++ 03findInTwoDimensionArray/divide-leftdown.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool Find(vector<vector<int> > array, int target)
+    {
+        int row = array.size(); 
+        int col = array[0].size();
+        bool isFound = false;
+        
+        for(int i = 0, j = col-1;(i >= 0 && i< row) && (j >= 0 && j < col);)
+        {
+            if(target == array[i][j])
+            {
+                isFound = true;
+                break;
+            }
+            else if(target < array[i][j])
+            {
+                cout << target << "<" << array[i][j] << "turn left" << endl;
+                j--;
+            }
+            else
+            {
+                cout << target << ">" << array[i][j] << "turn down" <<endl;
+                i++;
+            }
+        }
+        return isFound;  
+    }
+};
+
+int main()
+{
+    int a1[] = { 1, 2,  8,  9,};
+    int a2[] = { 2, 4,  9, 12,};
+    int a3[] = { 4, 7, 10, 13,};
+    int a4[] = { 6, 8, 11, 15,};
+    vector<vector<int> > array;
+    array.push_back(vector<int>(a1, a1 + 4));
+    array.push_back(vector<int>(a2, a2 + 4));
+    array.push_back(vector<int>(a3, a3 + 4));
+    array.push_back(vector<int>(a4, a4 + 4));
+
+    Solution solu;
+    cout << solu.Find(array, 7) << endl;
+    return 0;  
+}
