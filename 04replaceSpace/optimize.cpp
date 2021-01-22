b6e1cfa1ebf52ea4e760ceb880d2fe2bcf3cce86
Jolin-zhang <72132138+Jolin-zhang@users.noreply.github.com>
2021-01-22 11:16:38 +0800

    Update optimize.cpp
    adjust indent

--- 04replaceSpace/optimize.cpp
+++ 04replaceSpace/optimize.cpp
@@ -5,30 +5,30 @@ using namespace std;
 class Solution 
 {
 public:
- void replaceSpace(char *str,int length) 
- {
-     if (str == NULL || length <= 0) return; //判空
-     int cnt = 0;  // 空格的个数
-     for (int i=0; i != length; ++i) 
-     {
-         if (str[i] == ' ') ++cnt;
-     }
-     if (!cnt) return; // 没有空格，直接返回
-     int new_length = length + cnt * 2;
-     for (int i = length; i >= 0; --i) 
-     {
-         if (str[i] == ' ') 
+    void replaceSpace(char *str,int length) 
+    {
+         if (str == NULL || length <= 0) return; //判空
+         int cnt = 0;  // 空格的个数
+         for (int i=0; i != length; ++i) 
          {
-             str[new_length--] = '0';
-             str[new_length--] = '2';
-             str[new_length--] = '%';
+             if (str[i] == ' ') ++cnt;
          }
-         else 
+         if (!cnt) return; // 没有空格，直接返回
+         int new_length = length + cnt * 2;
+         for (int i = length; i >= 0; --i) 
          {
-             str[new_length--] = str[i];
+             if (str[i] == ' ') 
+             {
+                 str[new_length--] = '0';
+                 str[new_length--] = '2';
+                 str[new_length--] = '%';
+             }
+             else 
+             {
+                 str[new_length--] = str[i];
+             }
          }
      }
- }
 };
 
 int main()
