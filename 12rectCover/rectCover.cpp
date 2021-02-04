6235ed09de4835c1ca25b9e697d2451a64b424ee
Jolin-zhang <72132138+Jolin-zhang@users.noreply.github.com>
2021-02-04 14:32:53 +0800

    Update rectCover.cpp
    added code notes

--- 12rectCover/rectCover.cpp
+++ 12rectCover/rectCover.cpp
@@ -6,7 +6,7 @@ class Solution
 {
 public:
     int rectCover(int number) 
-    {   
+    {   //要么竖着放, 转化成f(n-1), 要么横着放两次, 转化成f(n-2),则f(n) = f(n-1) + f(n-2)
         int f = 0, g = 1, cnt = number + 1;
         if(number <= 0)
         {
